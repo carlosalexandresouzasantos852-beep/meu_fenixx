cb8dfab505734e506a26fbba57a57f1ef546c7ce
Carlos <carlos@gmail.com>
2025-12-21 20:32:13 -0300

    Removido keep_alive do run.py

--- meu_bot_farm/web.py
+++ meu_bot_farm/web.py
@@ -1,14 +1,15 @@
 from flask import Flask
 import threading
 
-app = Flask("")
+app = Flask('')
 
-@app.route("/")
+@app.route('/')
 def home():
-    return "Bot rodando!"
+    return "Bot online!"
 
 def run():
-    app.run(host="0.0.0.0", port=3000)  # Troque 3000 pela porta do Render
+    app.run(host='0.0.0.0', port=3000)
 
-# Rodar em uma thread separada para não travar o bot
-threading.Thread(target=run).start()
+def keep_alive():
+    t = threading.Thread(target=run)
+    t.start()
